edbf70d63a614abaf1baf67734e227a2101c31bb
Nick Taylor <nick.taylor@tutamail.com>
2022-02-06 15:58:15 -0500

    frontend stuff

--- src/EvansFreshRoast.Client/src/App.fs
+++ src/EvansFreshRoast.Client/src/App.fs
@@ -1,43 +1,88 @@
 ﻿module EvansFreshRoast.App
 
 open Feliz
+open Feliz.Bulma
 open Elmish
 open Elmish.React
 
 type State =
-    { Count: int }
+    { IsMainNavOpen: bool }
+
+type MainNavMsg =
+    | Opened
+    | Closed
 
 type Msg =
-    | Increment
-    | Decrement
+    | MainNav of MainNavMsg
 
 let init () =
-    { Count = 0 }, Cmd.none
+    { IsMainNavOpen = false }, Cmd.none
 
 let update (msg: Msg) (state: State) =
     match msg with
-    | Increment ->
-        { state with Count = state.Count + 1 }, Cmd.none
-
-    | Decrement ->
-        { state with Count = state.Count - 1 }, Cmd.none
+    | MainNav Opened ->
+        { state with IsMainNavOpen = true }, Cmd.none
+    
+    | MainNav Closed ->
+        { state with IsMainNavOpen = false }, Cmd.none
 
 let view (state: State) (dispatch: Msg -> unit) =
-    Html.div [
-        Html.p state.Count
-        Html.button [
-            prop.onClick (fun _ -> dispatch Increment)
-            prop.children [
-                Html.text "Click Me!"
+    Bulma.navbar [
+        Bulma.navbarBrand.div [
+            Bulma.navbarBrand.a [
+                prop.href "/"
+                prop.children [
+                    Html.text "Evan's Fresh Roast"
+                ]
+            ]
+
+            Bulma.navbarBurger [
+                if state.IsMainNavOpen then
+                    Bulma.navbarBurger.isActive
+                prop.onClick (fun _ ->
+                    match state.IsMainNavOpen with
+                    | true -> dispatch <| MainNav Closed
+                    | false -> dispatch <| MainNav Opened)
+                prop.children [
+                    for _ in 1..3 do
+                        Html.span [ prop.ariaHidden true ]
+                ]
             ]
         ]
-        Html.button [
-            prop.onClick (fun _ -> dispatch Decrement)
+        Bulma.navbarMenu [
+            if state.IsMainNavOpen then
+                Bulma.navbarMenu.isActive
             prop.children [
-                Html.text "NO, CLICK ME!"
+                Bulma.navbarStart.div [
+                    Bulma.navbarItem.a [
+                        prop.text "Roasts"
+                    ]
+                    Bulma.navbarItem.a [
+                        prop.text "Coffees"
+                    ]
+                    Bulma.navbarItem.a [
+                        prop.text "Customers"
+                    ]
+                ]
             ]
         ]
     ]
+    
+    // Bulma.block [
+    //     Bulma.box [
+    //         Html.form [
+    //             Bulma.field.div [
+    //                 Bulma.label "Phone Number"
+    //                 Bulma.control.div [
+    //                     Bulma.input.tel [
+    //                         prop.placeholder "111-222-3333"
+    //                     ]
+    //                 ]
+    //             ]
+    //         ]
+    //     ]
+    // ]
+    
 
 Program.mkProgram init update view
 |> Program.withReactBatched "app-root"
